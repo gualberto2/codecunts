205c6a9a7ef567d354f0ffeabc0c39f88ec225db
gualberto2 <elijahwilsonnn@gmail.com>
2023-03-14 11:32:45 -0700

    testing elastic accordian

---
+++ resources/js/javascript.js
@@ -0,0 +1,25 @@
+const items = document.querySelectorAll(".item");
+
+const expand = (item, i) => {
+  items.forEach((it, ind) => {
+    if (i === ind) return;
+    it.clicked = false;
+  });
+  gsap.to(items, {
+    width: item.clicked ? "15vw" : "8vw",
+    duration: 1,
+    ease: "power2"
+  });
+
+  item.clicked = !item.clicked;
+  gsap.to(item, {
+    width: item.clicked ? "42vw" : "15vw",
+    duration: 1,
+    ease: "power2"
+  });
+};
+
+items.forEach((item, i) => {
+  item.clicked = false;
+  item.addEventListener("click", () => expand(item, i));
+});
